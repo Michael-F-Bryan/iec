6e2348db0301a22cef801b34dfdc46d7863457d1
Michael Bryan <michaelfbryan@gmail.com>
2019-03-26 21:01:48 +0800

    We can parse assigning to a literal

--- syntax/src/ast.rs
+++ syntax/src/ast.rs
@@ -1,15 +1,16 @@
 use codespan::{ByteIndex, ByteSpan};
+use serde_derive::{Deserialize, Serialize};
 
 pub(crate) fn s(start: usize, end: usize) -> ByteSpan {
     ByteSpan::new(ByteIndex(start as u32), ByteIndex(end as u32))
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Statement {
     Declaration(Declaration),
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Identifier {
     pub value: String,
     pub span: ByteSpan,
@@ -24,7 +25,7 @@ impl Identifier {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Declaration {
     pub ident: Identifier,
     pub ty: Identifier,
@@ -41,14 +42,81 @@ impl Declaration {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Assignment {
+    pub variable: Identifier,
+    pub value: Expression,
+    pub span: ByteSpan,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum Expression {
+    Literal(Literal),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Literal {
+    pub kind: LiteralKind,
+    pub span: ByteSpan,
+}
+
+impl Literal {
+    pub fn new<K: Into<LiteralKind>>(kind: K, span: ByteSpan) -> Literal {
+        Literal {
+            kind: kind.into(),
+            span,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum LiteralKind {
+    Boolean(bool),
+    Integer(i64),
+    Float(f64),
+    String(String),
+}
+
+impl From<bool> for LiteralKind {
+    fn from(other: bool) -> LiteralKind {
+        LiteralKind::Boolean(other)
+    }
+}
+
+impl From<i64> for LiteralKind {
+    fn from(other: i64) -> LiteralKind {
+        LiteralKind::Integer(other)
+    }
+}
+
+impl From<f64> for LiteralKind {
+    fn from(other: f64) -> LiteralKind {
+        LiteralKind::Float(other)
+    }
+}
+
+impl From<String> for LiteralKind {
+    fn from(other: String) -> LiteralKind {
+        LiteralKind::String(other)
+    }
+}
+
+impl<'a> From<&'a str> for LiteralKind {
+    fn from(other: &'a str) -> LiteralKind {
+        LiteralKind::String(other.to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
 
     macro_rules! parse_test {
         ($name:ident, $parser:tt, $input:expr => $expected:expr) => {
             #[test]
             fn $name() {
+                #[allow(unused_imports)]
                 use $crate::grammar::*;
                 let got = $parser::new().parse($input).unwrap();
                 assert_eq!(got, $expected);
@@ -63,5 +131,14 @@ mod tests {
         ident: Identifier::new("x", s(0, 1)),
         ty: Identifier::new("Bool", s(3, 7)),
         span: s(0, 7),
+    });
+
+    parse_test!(assign_literal, AssignmentParser, "meaning_of_life := 42" => Assignment {
+        variable: Identifier::new("meaning_of_life", s(0, 15)),
+        value: Expression::Literal(Literal {
+            kind: LiteralKind::Integer(42),
+            span: s(19, 21),
+        }),
+        span: s(0, 21),
     });
 }
--- syntax/src/lib.rs
+++ syntax/src/lib.rs
@@ -1,10 +1,9 @@
 #[macro_use]
 extern crate lalrpop_util;
-#[test]
-#[macro_use]
-extern crate pretty_assertions;
 
-pub mod ast;
+mod ast;
+
+pub use crate::ast::*;
 
 lalrpop_util::lalrpop_mod!(
     #[allow(dead_code)]
