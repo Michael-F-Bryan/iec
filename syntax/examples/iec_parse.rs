46b8df56a74a47fc08cc37cb018ac96d35df50b6
Michael Bryan <michaelfbryan@gmail.com>
2019-03-31 15:40:22 +0800

    Upgraded deps and made the example also generate JSON output

--- syntax/examples/iec_parse.rs
+++ syntax/examples/iec_parse.rs
@@ -1,6 +1,7 @@
 use iec_syntax::File;
 use std::io::{self, Read};
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 use structopt::StructOpt;
 
 fn main() {
@@ -12,10 +13,21 @@ fn main() {
 
     let file: File = buffer.parse().unwrap();
 
-    println!("{:#?}", file);
+    match args.format {
+        OutputFormat::Rust => println!("{:#?}", file),
+        OutputFormat::Json => {
+            serde_json::to_writer(io::stdout(), &file).unwrap()
+        }
+        OutputFormat::PrettyJson => {
+            serde_json::to_writer_pretty(io::stdout(), &file).unwrap()
+        }
+    }
 }
 
 #[derive(StructOpt)]
+#[structopt(
+    about = "Parse some Structured Text into the corresponding Abstract Syntax Tree"
+)]
 struct Args {
     #[structopt(
         default_value = "-",
@@ -23,6 +35,14 @@ struct Args {
         help = "The file to read (defaults to stdin)"
     )]
     file: PathBuf,
+    #[structopt(
+        short = "f",
+        long = "format",
+        default_value = "rust",
+        raw(possible_values = "&[\"rust\", \"json\", \"pretty\"]"),
+        help = "The format to use when displaying the AST"
+    )]
+    format: OutputFormat,
 }
 
 impl Args {
@@ -32,6 +52,28 @@ impl Args {
         } else {
             std::fs::File::open(&self.file)
                 .map(|f| Box::new(f) as Box<dyn Read>)
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum OutputFormat {
+    Json,
+    PrettyJson,
+    Rust,
+}
+
+impl FromStr for OutputFormat {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lowercase = s.to_lowercase();
+
+        match lowercase.as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "pretty" => Ok(OutputFormat::PrettyJson),
+            "rust" => Ok(OutputFormat::Rust),
+            _ => Err("Expected a valid output format"),
         }
     }
 }
