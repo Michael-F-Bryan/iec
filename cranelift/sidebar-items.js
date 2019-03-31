6d8e0c0ec198fb898e9f9190eaddf5222b8f0a44
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-31 07:49:18 +0000

    Deploy Michael-F-Bryan/iec to github.com/Michael-F-Bryan/iec.git:gh-pages

--- cranelift/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"constant":[["VERSION","Version number of this crate."]],"mod":[["prelude","A prelude providing convenient access to commonly-used cranelift features. Use as `use cranelift::prelude::*`."]]});
--- cranelift_codegen/ir/function/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["DisplayFunction","Wrapper type capable of displaying a `Function` with correct ISA annotations."],["Function","A function."],["InstOffsetIter","Iterator returning instruction offsets and sizes: `(offset, inst, size)`."]]});
